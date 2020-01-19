cce747c6009fea1b8ac6eb1bc0f4619c1fb52948
Kyle <43049771+Crisiskyle@users.noreply.github.com>
2020-01-18 23:00:11 -0500

    Delete Kyle.js

--- Kyle.js
+++
@@ -1,9 +0,0 @@
-const Person = require("./Person.js");
-
-class Kyle extends Person {
-    constructor(name, favoriteColor, hobby){
-        super(name, favoriteColor);
-        this.hobby = hobby;
-    }
-}
-module.exports = Kyle;
