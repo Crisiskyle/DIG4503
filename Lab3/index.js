d1ae157c53fc88b3ce871e3c4a9655cb8098cb2b
Kyle <43049771+Crisiskyle@users.noreply.github.com>
2020-01-29 19:41:29 -0500

    NEw

--- Lab3/index.js
+++ Lab3/index.js
@@ -3,6 +3,10 @@ const App=Express();
 const port =8080;
 App.use(Express.static("public"));
 
+App.get("/public",function(req,res){
+    res.send("Hello , WORLD");
+});
+
 App.listen(port,function(){
     console.log("Server Running!");
 
