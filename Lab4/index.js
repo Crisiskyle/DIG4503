a22cba8d3772770b7f7ab24efb069536112e9c1e
Kyle <43049771+Crisiskyle@users.noreply.github.com>
2020-02-08 21:31:07 -0500

    Project 4 1

---
+++ Lab4/index.js
@@ -0,0 +1,30 @@
+
+const Express = require("express");
+const App = Express();
+const port = 80;
+const pokemon = require("json-pokemon");
+const chalk=require("chalk");
+
+App.get("/id/:id",function(req,res){
+    pokemon.forEach((value)=> {
+        if(value.id == req.params.id) {
+            res.send(req.params);
+        }       
+});
+    res.send("Invalid!");
+    console.log(chalk.red("invalid"));
+});
+
+App.get("/name/:name",function(req,res) {
+   pokemon.forEach((value)=> {
+        if(value.name == req.params.name) {
+            res.send(req.params);
+        }
+    });
+    res.send("Invalid!");
+    console.log(chalk.red("invalid"));
+});
+
+App.listen(port, () => {
+    console.log("Server running!");
+});
