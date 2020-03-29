c2fac7d2da558d6bf8ce6953af5e3145b2c996c6
Kyle <43049771+Crisiskyle@users.noreply.github.com>
2020-03-28 23:51:39 -0400

    Commit

--- lab9/components/IdSearch/index.js
+++ lab9/components/IdSearch/index.js
@@ -6,16 +6,13 @@ class IdSearch extends React.Component {
 
     IdSearchFormSubmit = () => {
 
-        // Get the input's value
         let idSearch = document.querySelector('#idSearch').value;
 
-        // Look for pokemon based on the id
         fetch("http://localhost:3000/api/pokemon/id/" + idSearch)
         .then((res) => { return res.json() })
         .then((processed) => {
             console.log(processed);
-            // Call the callback function given to the class component
-            //this.props.callback(processed);
+          
             let reporting = document.querySelector("#reportingArea");
 
             if(processed.error){
